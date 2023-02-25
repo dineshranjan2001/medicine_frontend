782f9d297dfed33b7b0db23edf75cdcab624ae0a
Dinesh <dineshranjanbiswal70@gmail.com>
2023-02-25 19:19:42 +0530

    sign-up design complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
-  path:"sign-in",
-  component:SignInComponent,
-  pathMatch:"full"
+    path: "sign-in",
+    component: SignInComponent,
+    pathMatch: "full"
   },
+  {
+    path:"sign-up",
+    component:SignUpComponent,
+    pathMatch:"full"
+  }
 ];
 
 @NgModule({
