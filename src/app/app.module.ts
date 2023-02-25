9070fb9b39f838118db572d6f90e6c3357be637e
Dinesh <dineshranjanbiswal70@gmail.com>
2023-02-25 12:29:54 +0530

    navbar and sign-in component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
